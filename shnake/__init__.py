c110a32244c5069992fd4af8bdfeff64e16da396
nil0x42 <nil0x42@gmail.com>
2014-04-06 22:22:59 +0200

    started a shnake test for new proxy object by unheritance

--- shnake/__init__.py
+++ shnake/__init__.py
@@ -1,2 +1,17 @@
-from .parser import Parser
-from .shell import Cmd
+from . import shell
+try:
+    from .parser import Parser
+except ImportError:
+    Parser = None;
+
+class Cmd(shell.Cmd):
+
+    def __init__(self, completekey='tab', stdin=None, stdout=None):
+        super().__init__(completekey=completekey, stdin=stdin, stdout=stdout)
+
+    def parseline(self, string, interactive=True):
+        return super().parseline(string=string, interactive=interactive)
+        # if Parser is None:
+        #     super().parseline(string=string, interactive=interactive)
+        # else
+        #     parser = Parser()
